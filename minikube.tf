34d4e300b7ae16ddbd5602b7de03782f8fad5247
nikhil <anc@gmail.com>
2023-11-11 15:11:41 +0530

    all

--- minikube.tf
+++ minikube.tf
@@ -4,7 +4,7 @@ module "minikube" {
   aws_region    = "us-east-1"
   cluster_name  = "roboshop"
   aws_instance_type = "t3.medium"
-  ssh_public_key = "C:/Users/Asus/Desktop/ssh/chandra"
+  ssh_public_key = "~/Desktop/ssh/chandra.pub"
   aws_subnet_id = "	subnet-04cc2f385e219a7f5"
 #   ami_image_id = "ami-b81dbfc5"
   hosted_zone = "joindevops.cloud"
