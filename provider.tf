e5b6a82a867b133543ceb14c51237781bfbce32a
nikhil <anc@gmail.com>
2023-11-12 18:20:53 +0530

    all

--- provider.tf
+++ provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "5.25.0"
     }
   }
-  # backend "s3" {
-  #   bucket   = "roboshop-locking"
-  #   key = "minikube"
-  #   region = "us-east-1"
-  #   dynamodb_table = "roboshop-state-lock"
-  # }
+  backend "s3" {
+    bucket   = "roboshop-locking"
+    key = "minikube"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-state-lock"
+  }
 }
 
 provider "aws" {
